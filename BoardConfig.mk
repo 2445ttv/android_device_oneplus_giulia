Tuxluxury/android_device_oneplus_waffle

waffle: Setup regional system props
Change-Id: I8ba8df188fc13f3e18120560537a6cbc18c54c6b
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

BoardConfig.mk
@@ -19,6 +19,7 @@ TARGET_SCREEN_DENSITY := 640
 TARGET_KERNEL_CONFIG += vendor/oplus/waffle.config
 
 # Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery

2445ttv/android_device_oneplus_giulia

BoardConfig.mk
@@ -19,6 +19,7 @@ TARGET_SCREEN_DENSITY := 640
 TARGET_KERNEL_ADDITIONAL_FLAGS += CONFIG_WAFFLE_DTB=y
 
 # Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 